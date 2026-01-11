832197809748eaaf082f9160d681a255853cb37c
Indufoundation <maindrawatidevifoundation@gmail.com>
2026-01-11 17:17:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -56,4 +56,28 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected successfully"))
   .catch(err => console.error("MongoDB connection error:", err));
 const mongoose = require('mongoose');
+mongodb+srv://Maindrawatidevifoundation:<db_password>@cluster0.wql8whp.mongodb.net/?appName=Cluster0
+const { MongoClient } = require('mongodb');
 
+const uri = "mongodb+srv://Maindrawatidevifoundation:<db_password>@cluster0.wql8whp.mongodb.net/?appName=Cluster0
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("MongoDB connected successfully");
+
+    const database = client.db("myDatabase");
+    const collection = database.collection("members");
+
+    // Example: fetch all members
+    const members = await collection.find({}).toArray();
+    console.log(members);
+
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+    
