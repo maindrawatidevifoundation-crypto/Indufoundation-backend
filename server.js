7e3e25804803419675bf03c71242c23472f495be
Indufoundation <maindrawatidevifoundation@gmail.com>
2026-01-20 15:37:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -130,4 +130,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-  
+  app.use("/activities", require("./routes/activities"));
+              
