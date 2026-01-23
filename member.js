2fd1f10cb574c89bc1c03d7a3e6d5bb8217b22af
Indufoundation <maindrawatidevifoundation@gmail.com>
2026-01-23 12:07:06 +0530

    Update member.js

--- member.js
+++ member.js
@@ -1,10 +1,11 @@
+// member.js
 const mongoose = require("mongoose");
 
 const memberSchema = new mongoose.Schema({
   name: { type: String, required: true },
   mobile: { type: String },
   interest: { type: String },
-  memberId: { type: String } // <- This is REQUIRED to save IDs
-});
+  memberId: { type: String } // ✅ official ID
+}, { timestamps: true });
 
 module.exports = mongoose.model("Member", memberSchema);
